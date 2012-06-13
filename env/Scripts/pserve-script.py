4daae05b974a3ea376446d10ec7c4ce3d56a8fef
Joe Dallago <jd.dallago@gmail.com>
2012-06-12 23:51:20 -0700

    Added all of the files necessary to generate the setup.exe

---
+++ env/Scripts/pserve-script.py
@@ -0,0 +1,9 @@
+#!C:\Users\VAMP\Documents\GitHub\kiya\env\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'pyramid==1.3.2','console_scripts','pserve'
+__requires__ = 'pyramid==1.3.2'
+import sys
+from pkg_resources import load_entry_point
+
+sys.exit(
+   load_entry_point('pyramid==1.3.2', 'console_scripts', 'pserve')()
+)
---
+++ env/Scripts/unit2-script.py
@@ -0,0 +1,9 @@
+#!C:\Users\VAMP\Documents\GitHub\kiya\env\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'unittest2==0.5.1','console_scripts','unit2'
+__requires__ = 'unittest2==0.5.1'
+import sys
+from pkg_resources import load_entry_point
+
+sys.exit(
+   load_entry_point('unittest2==0.5.1', 'console_scripts', 'unit2')()
+)
