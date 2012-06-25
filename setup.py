85bdfc73ffc22b4e47fc51a58f2725ef4b086f24
Joe Dallago <jd.dallago@gmail.com>
2012-06-25 01:16:34 -0700

    Made a few changes to the setup.py file.

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 from distutils.core import setup
 import py2exe
 
+requires = []
+
 setup(
+    name='kiya',
+    version='0.1',
+    packages=['kiya'],
+    requires=requires,
     console=['kiya/base.py'],
-    options = {
-        'py2exe' : {
+    options={
+        'py2exe': {
           'packages': [],
-          'includes': ['cairo', 
-                       'pygtk', 
-                       'gio', 
-                       'pango', 
-                       'pangocairo', 
-                       'atk', 
+          'includes': ['cairo',
+                       'pygtk',
+                       'gio',
+                       'pango',
+                       'pangocairo',
+                       'atk',
                        'gobject']
         },
         'sdist': {
