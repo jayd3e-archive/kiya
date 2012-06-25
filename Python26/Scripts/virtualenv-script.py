b330747369a7a6ac367382ca2d76248c03900ae2
Joe Dallago <jd.dallago@gmail.com>
2012-06-24 22:28:14 -0700

    Starting anew, with a new process and pygtk.

--- Python26/Scripts/virtualenv-script.py
+++
@@ -1,9 +0,0 @@
-#!C:\Users\VAMP\Documents\GitHub\kiya\Python26\python.exe
-# EASY-INSTALL-ENTRY-SCRIPT: 'virtualenv==1.7.1.2','console_scripts','virtualenv'
-__requires__ = 'virtualenv==1.7.1.2'
-import sys
-from pkg_resources import load_entry_point
-
-sys.exit(
-   load_entry_point('virtualenv==1.7.1.2', 'console_scripts', 'virtualenv')()
-)
--- env/Scripts/prequest-script.py
+++
@@ -1,9 +0,0 @@
-#!C:\Users\VAMP\Documents\GitHub\kiya\env\Scripts\python.exe
-# EASY-INSTALL-ENTRY-SCRIPT: 'pyramid==1.3.2','console_scripts','prequest'
-__requires__ = 'pyramid==1.3.2'
-import sys
-from pkg_resources import load_entry_point
-
-sys.exit(
-   load_entry_point('pyramid==1.3.2', 'console_scripts', 'prequest')()
-)
