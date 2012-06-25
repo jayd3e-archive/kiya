6f4ac847935b2baaf9aaef8beda8598d67ab173c
Joe Dallago <jd.dallago@gmail.com>
2012-06-24 23:57:40 -0700

    Have a basic proof of concept working with py2exe.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+import py2exe
+
+setup(
+    console=['kiya/base.py'],
+    options = {
+        'py2exe' : {
+          'packages': [],
+          'includes': ['cairo', 
+                       'pygtk', 
+                       'gio', 
+                       'pango', 
+                       'pangocairo', 
+                       'atk', 
+                       'gobject']
+        },
+        'sdist': {
+          'formats': 'zip'
+        }
+    }
+)
